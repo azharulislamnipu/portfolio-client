776c4db4406d5eae452f095a96ca90268c2cc3bf
Puredevs <azhar@puredevs.com>
2019-10-10 17:46:30 +0600

    Header Added

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../layouts/Header';
+export default function Home() {
+    return (
+        <div>
+        <Header/> 
+        <h2>Home page</h2>
+        </div>
+    )
+}
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -27,7 +27,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 8080
+    port: 3000
   },
   devtool:'sourcemap',
   module: {

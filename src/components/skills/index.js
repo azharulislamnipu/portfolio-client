eb9c8ed65caf3fd798d1cbb4ac07d1e313d07f95
azhar <azharulislamnipu@gmail.com>
2019-11-01 01:24:02 +0600

    skills add

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -10,39 +10,106 @@ class Skills extends Component {
 
     return (
       <section className="skills-area">
-         <Container>
-         {<Title title='Skills' headline='Technical Skills' align='text-right' position='justify-content-end' style={{headingcolor:'#1f2235',titlecolor:'#666ce0'}} /> }
-         <Row className='skrill_row'>
-                <div className="col-lg-4">
-                    <h4 className="heding">Extra skills</h4>
-                    <ul className="extra_skills">
-                        <li>LOGO DESIGN</li>
-                        <li>PHOTOGRAPHY</li>
-                        <li>Application Development</li>
-                        <li>Software Development</li>
-                        <li>Illustrator</li>
-                        <li>USER EXPERIENCE</li>
-                        <li>AUDIO VIDEO EDITING</li>
-                    </ul>
-                </div>
-                <div className="col-lg-8 skrill">  
-                    <h4>PHOTOSHOP</h4>
+        <Container>
+          {<Title title='Skills' headline='Technical Skills' align='text-right' position='justify-content-end' style={{ headingcolor: '#1f2235', titlecolor: '#666ce0' }} />}
+          <Row className='skrill_row'>
+            <Col lg={4} md={6} sm={12}>
+              <h4 className="heding">Extra skills</h4>
+              <ul className="extra_skills">
+                <li>LOGO DESIGN</li>
+                <li>PHOTOGRAPHY</li>
+                <li>Application Development</li>
+                <li>Software Development</li>
+                <li>Illustrator</li>
+                <li>USER EXPERIENCE</li>
+                <li>AUDIO VIDEO EDITING</li>
+              </ul>
+            </Col>
+            <Col lg={8} md={6} sm={12} className="skrill">
 
-                    <div className="progress-wrrap  fourtypercent">
-                      <div className="progress-title">40%</div>
-                       <ProgressBar  now={40} />
-                    </div>
-                  
-                   
-                    <h4>HTML5 / CSS3</h4>
-                   
-                
-                  
-              
+              <div className="single-skille">
+                <h4>PHOTOSHOP</h4>
 
+                <div className="progress-wrrap  fourtypercent">
+                  <div className="progress-title">40%</div>
+                  <ProgressBar now={40} />
                 </div>
-         </Row>
-         </Container>
+              </div>
+              <div className="single-skille">
+                <h4>HTML</h4>
+
+                <div className="progress-wrrap  fiftypercent">
+                  <div className="progress-title">50%</div>
+                  <ProgressBar now={50} />
+                </div>
+              </div>
+
+              <div className="single-skille">
+
+                <h4>JAVASCRIPT</h4>
+
+                <div className="progress-wrrap  ninetypercent">
+                  <div className="progress-title">90%</div>
+                  <ProgressBar now={90} />
+                </div>
+              </div>
+
+              <div className="single-skille ">
+
+                <h4>React JS</h4>
+
+                <div className="progress-wrrap  ninetyfivepercent">
+                  <div className="progress-title">95%</div>
+                  <ProgressBar now={90} />
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row className='skrill_row2'>
+            <Col lg={6} md={6} sm={12}>
+
+              <h4 className="heding">Programming Skills</h4>
+
+              <div className="single-skille">
+                <h4>PHOTOSHOP</h4>
+
+                <div className="progress-wrrap  fourtypercent">
+                  <div className="progress-title">40%</div>
+                  <ProgressBar now={40} />
+                </div>
+              </div>
+              <div className="single-skille">
+                <h4>HTML</h4>
+
+                <div className="progress-wrrap  fiftypercent">
+                  <div className="progress-title">50%</div>
+                  <ProgressBar now={50} />
+                </div>
+              </div>
+            </Col>
+            <Col lg={6} md={6} sm={12}>
+
+              <h4 className="heding">language Skills</h4>
+
+              <div className="single-skille">
+                <h4>PHOTOSHOP</h4>
+
+                <div className="progress-wrrap  fourtypercent">
+                  <div className="progress-title">40%</div>
+                  <ProgressBar now={40} />
+                </div>
+              </div>
+              <div className="single-skille">
+                <h4>HTML</h4>
+
+                <div className="progress-wrrap  fiftypercent">
+                  <div className="progress-title">50%</div>
+                  <ProgressBar now={50} />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </section>
     )
   }
